0aed9ef81c45aac1e8acfcd77367eadc54113151
santosh-dotcom <61147345+santosh-dotcom@users.noreply.github.com>
2023-08-30 08:29:09 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+// object
+
+// console.log(typeof "deepak")
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
+
