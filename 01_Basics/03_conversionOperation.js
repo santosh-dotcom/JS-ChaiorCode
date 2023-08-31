8e1e780570146c60132b5a8bd7e52254a471bce7
santosh-dotcom <61147345+santosh-dotcom@users.noreply.github.com>
2023-08-31 16:00:54 +0000

    Conversion Operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33"
+// let score = "33abc" //NaN
+// let score = null
+// let score = undefined
+let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+// "" => false
+//"hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
