8b20bf8bb9f4d897e104efdc9277ef26e181f6a4
santosh-dotcom <61147345+santosh-dotcom@users.noreply.github.com>
2023-09-08 12:16:05 +0000

    Functions basic 2

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -30,4 +30,35 @@ function loginUserMessage(userName = "sam"){
     return `${userName} just logged in`
 }
 
-console.log(loginUserMessage("santosh"))
+// console.log(loginUserMessage("santosh"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(299, 399, 499))
+
+const user = {
+    userName: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.userName} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 300, 100, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 100, 500]))
