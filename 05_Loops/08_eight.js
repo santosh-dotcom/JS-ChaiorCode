307e39be262c0856be595136d7ce511c72dd9a43
santosh-dotcom <61147345+santosh-dotcom@users.noreply.github.com>
2023-09-15 03:40:40 +0000

    Basic Loops

---
+++ 05_Loops/08_eight.js
@@ -0,0 +1,37 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 5)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "Js course",
+        price: 2999
+    },
+
+    {
+        itemName: "py course",
+        price: 999
+    },
+
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceTopay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceTopay);
